5b6f9dc712a8c19051d9bebb930c4d7b94fa9f7b
KalpakRamidwar <159031848+KalpakRamidwar@users.noreply.github.com>
2025-07-08 03:19:22 +0000

    data types

---
+++ .devcontainer/basics/2_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all js code as newer version
+
+// aler(3+3) give error becouse we are using node.js not browser
+
+console.log(3+3)
+
+let name="Kalpak" //string
+let age=20 //number
+let isloggedIn=true // boolean
+let state=null
+
+// number=> range is 2 to power 53
+// bigint
+// String
+// boolean
+// null => standalone value
+// undefined 
+// symbol => unique
+
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
