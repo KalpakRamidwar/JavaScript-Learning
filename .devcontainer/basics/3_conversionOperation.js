31570b2f318fcb80e070684aa544c143f0d5f1f2
KalpakRamidwar <159031848+KalpakRamidwar@users.noreply.github.com>
2025-07-08 03:51:38 +0000

    conversion of datatypes

---
+++ .devcontainer/basics/3_conversionOperation.js
@@ -0,0 +1,27 @@
+let score="33abc"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber=Number(score)
+// console.log(typeof score)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "Kalpak"=> NaN (not a number)
+// true =1 and false=0
+
+
+let isLoggedIn=1
+let booleanIsLoggedIn=boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1= true and 0=false
+
+// ""= false
+//"anything"=true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
