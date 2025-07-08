19c628a0f15516e58d0f0b1a0b88cdd6a010d505
KalpakRamidwar <159031848+KalpakRamidwar@users.noreply.github.com>
2025-07-08 16:09:58 +0000

    camparison operators

---
+++ .devcontainer/basics/4_comparision.js
@@ -0,0 +1,25 @@
+// comparision operators
+
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// avoid compairing with different datatypes 
+// while compairing type of data should be same
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined<0);
+
+// === will strictly checks
+console.log("2"===2);
