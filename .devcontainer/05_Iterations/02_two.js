19c5c45797a6b288569988ab1979072255ea66a4
Kalpak Ramidwar <159031848+KalpakRamidwar@users.noreply.github.com>
2025-07-12 17:11:19 +0000

    while and do while loop

---
+++ .devcontainer/05_Iterations/02_two.js
@@ -0,0 +1,20 @@
+// while and do while loop
+
+let index=0
+// while (index<10) {
+//     console.log(`value of index ${index}`)
+//     index=index+2
+// }
+
+// let myArr=["kalpak","pranay","vihang"]
+// let arr=0
+// while(arr<myArr.length){
+//     console.log(`value is ${myArr[arr]}`)
+//     arr++
+// }
+
+let score=1
+do {
+    console.log(`score is ${score}`)
+    score++
+} while (score<10);
